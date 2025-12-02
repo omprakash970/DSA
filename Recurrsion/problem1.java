21e2a64cc9163107bd4ec3be2a67994eb4bc929f
Bandi Om Prakash <omprakashbandi583@gmail.com>
2025-12-02 14:08:46 +0530

    done

--- Recurrsion/problem1.java
+++ Recurrsion/problem1.java
@@ -1,1 +1,27 @@
-//tailing problem in java 
+// Tiling problem in Java (2xN floor with 2x1 tiles)
+public class problem1 {
+    static int tailingProblem(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        int verticalTails = tailingProblem(n - 1);
+        int horizontalTails = tailingProblem(n - 2);
+        return verticalTails + horizontalTails;
+        /*
+        Dry Run (n=4):
+        tailingProblem(4)
+        = tailingProblem(3) + tailingProblem(2)
+        = (tailingProblem(2) + tailingProblem(1)) + (tailingProblem(1) + tailingProblem(0))
+        = ((tailingProblem(1) + tailingProblem(0)) + 1) + (1 + 1)
+        = ((1 + 1) + 1) + (1 + 1)
+        = (2 + 1) + 2
+        = 3 + 2
+        = 5
+        */
+    }
+
+    public static void main(String[] args) {
+        int n = 4; // length of the floor
+        System.out.println(tailingProblem(n));
+    }
+}
---
+++ Recurrsion/problem2.java
@@ -0,0 +1,7 @@
+//remove duplicates from a string using recursion
+
+public class problem2 {
+    
+
+    
+}
