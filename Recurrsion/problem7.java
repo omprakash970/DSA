509720277cf75721f22d3426b4f7acc669aac198
Bandi Om Prakash <omprakashbandi583@gmail.com>
2025-12-02 17:36:22 +0530

    done

--- Recurrsion/problem7.java
+++ Recurrsion/problem7.java
@@ -1,6 +1,10 @@
-//WearegivenastringS,weneedtofindthecountofallcontiguoussubstringsstarting and ending with the same character
+/**
+ * Given a string s, count all contiguous substrings that start and end with the same character.
+ * Each single character counts. For a character occurring f times, it contributes f*(f+1)/2 substrings.
+ */
 
 
 public class problem7 {
+    
     
 }
