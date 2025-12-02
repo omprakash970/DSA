e526c98447455ff5e80c207d32ed50b881c2f5a4
Bandi Om Prakash <omprakashbandi583@gmail.com>
2025-12-02 14:19:38 +0530

    done

--- Recurrsion/problem2.java
+++ Recurrsion/problem2.java
@@ -1,7 +1,25 @@
 //remove duplicates from a string using recursion
 
 public class problem2 {
-    
+    public static void removeDuplicates(String str, int idx, StringBuilder newstr, boolean [] map){
+        if(idx==str.length()){
+            System.out.println(newstr);
+            return;
+        }
+        char currChar=str.charAt(idx); 
+        if(map[currChar-'a']==true){
+            removeDuplicates(str, idx, newstr, map);
+        }else{
+            map[currChar-'a']=true;
+            removeDuplicates(str, idx+1, newstr.append(currChar), map);
+        }
+
+    }
+    public static void main(String [] args){
+        String str="omprakash"; 
+        removeDuplicates(str, 0, new StringBuilder(""), new boolean[26]);
+    }
+
 
     
 }
