7e1f81c4a5b58164a9cfe9e99172d570c43fabe8
Bandi Om Prakash <omprakashbandi583@gmail.com>
2025-12-04 13:15:17 +0530

    done

--- ArrayList/operations.java
+++ ArrayList/operations.java
@@ -10,8 +10,26 @@ public class operations {
         list.add(3);
         System.out.println(list);
         //get
-        int element= list.get(1); 
-        System.out.println(element);
+        System.out.println(list.get(0));
+        //remove element: 
+        list.remove(1);
+        System.out.println(list);
+        //set element
+        list.set(0, 5);
+        System.out.println(list);
+        //contains element 
+        System.out.println(list.contains(3));
+        //size
+        System.out.println(list.size());
+        //reverse
+        System.out.println(list.reversed());
+        //find maximum 
+       int max=Integer.MIN_VALUE;
+       for(int i=0;i<list.size();i++){
+        if(list.get(i)>max){
+            max=list.get(i);
+        }
+       }
     }
 
 }
