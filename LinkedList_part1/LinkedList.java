90da27e3f5942902a1dfb8e50610146b7bd4efb7
Bandi Om Prakash <omprakashbandi583@gmail.com>
2025-12-08 21:16:00 +0530

    done

---
+++ LinkedList_part1/LinkedList.java
@@ -0,0 +1,24 @@
+package LinkedList_part1;
+
+public class LinkedList {
+    public static class Node{
+        int data; 
+        Node next; 
+        Node(int data){
+            this.data=data; 
+            this.next=null;
+        }
+    }
+    public static Node head; 
+    public static Node tail; 
+    public static void main(String[] args) {
+     intro ll=new intro();
+        ll.head =new Node(1);
+        ll.head.next=new Node(2);
+        ll.head.next.next=new Node(3);
+        ll.head.next.next.next=new Node(4);
+        
+    }   
+
+    
+}
