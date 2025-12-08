a560074cee49e5c0582273b4892fc7e9dcdbf44f
Bandi Om Prakash <omprakashbandi583@gmail.com>
2025-12-08 21:09:07 +0530

    done

---
+++ LinkedList_part1/intro.java
@@ -0,0 +1,17 @@
+package LinkedList_part1;
+class Node{
+    int data; 
+    Node next; 
+    Node(int data){
+        this.data=data; 
+        this.next=null;
+    }
+
+}
+public class intro {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Linked List");
+    }   
+
+    
+}
