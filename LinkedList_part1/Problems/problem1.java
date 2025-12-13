214b962654bdccd5b44d5cc6941814ccfdb11417
Bandi Om Prakash <omprakashbandi583@gmail.com>
2025-12-13 21:48:30 +0530

    done

--- LinkedList_part1/Problems/problem1.java
+++ LinkedList_part1/Problems/problem1.java
@@ -1,3 +1,16 @@
 public class problem1 {
+    public static class  Node{
+        int data; 
+        Node next; 
+        Node(int data){
+            this.data=data; 
+            this.next=null;
+        }
+
+    }
+    public Node getIntersectionNode(Node headA, Node headB){
+
+    }
+
     
 }
