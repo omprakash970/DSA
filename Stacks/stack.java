48dad49037178ac4c4e4453920f3bd66d79bf152
Bandi Om Prakash <omprakashbandi583@gmail.com>
2025-12-13 22:09:06 +0530

    yoooooooo

--- Stacks/stack.java
+++ Stacks/stack.java
@@ -11,6 +11,15 @@ public class stack {
         public void push(int data){
             list.add(data); 
         }
+        public static int pop(){
+            if(isEmpty()){
+                return -1; 
+            }
+            int top=list.get(list.size()-1);
+            list.remove(list.size()-1);
+            return top;
+        }
+
     }
     
 }
