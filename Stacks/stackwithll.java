6cbd581feeda6a6716f1432f7b751397aa8e2dac
Bandi Om Prakash <omprakashbandi583@gmail.com>
2025-12-13 22:14:33 +0530

    completed

--- Stacks/stackwithll.java
+++ Stacks/stackwithll.java
@@ -1,5 +1,28 @@
 package Stacks;
 
 public class stackwithll {
+    public static class Node{
+        int data; 
+        Node next; 
+        Node(int data){
+            this.data=data; 
+            this.next=null;
+        }
+    }
+    static class Stack{
+        static Node head = null; 
+        public static boolean isEmpty(){
+            return head==null;
+        }
+        public static void push(int data){
+            Node newNode = new Node(data); 
+            if(isEmpty()){
+                head=newNode; 
+                return; 
+            }
+            newNode.next=head; 
+            head=newNode;
+        }
+    }
     
 }
