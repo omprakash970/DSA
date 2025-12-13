06cdfa68287bb62806e0704a2ab794a4bf1f4531
Bandi Om Prakash <omprakashbandi583@gmail.com>
2025-12-13 21:46:06 +0530

    added

--- LinkedList_part1/LinkedList.java
+++ LinkedList_part1/LinkedList.java
@@ -115,11 +115,30 @@ public class LinkedList {
         Node newLeft=mergeSort(head);
         Node newRight=mergeSort(rightHead);
         //Merge
+        return merge(newLeft, newRight);
 
-        return mergeSort(newLeft, newRight);
-        
     }
-
+    private Node merge(Node head1, Node head2){
+        Node mergedLL= new Node(-1);
+        Node temp=mergedLL;
+        while(head1!=null && head2!=null){
+            if(head1.data<=head2.data){
+                temp.next=head1; 
+                head1=head1.next; 
+                temp=temp.next;
+            }else{
+                temp.next=head2; 
+                head2=head2.next; 
+                temp=temp.next;
+            }
+        }
+        if(head1!=null){
+            temp.next=head1;
+        }else{
+            temp.next=head2;
+        }
+        return mergedLL.next;
+    }
 
 
     public static void main(String[] args) {
