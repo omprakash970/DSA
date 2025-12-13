5c74935682b7230fba341c22e8c2059f1595ac75
Bandi Om Prakash <omprakashbandi583@gmail.com>
2025-12-13 22:03:57 +0530

    done

---
+++ Stacks/stack.java
@@ -0,0 +1,13 @@
+package Stacks;
+
+import java.util.ArrayList;
+
+public class stack {
+    public static class  stacka{
+        static ArrayList<Integer> list = new ArrayList<>();
+        public boolen isEmpty(){
+            return list.size()==0;
+        }
+    }
+    
+}
