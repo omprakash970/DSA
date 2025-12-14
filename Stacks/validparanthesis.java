8cc63824d334ab3d6bca120c54bf287b7995e360
Bandi Om Prakash <omprakashbandi583@gmail.com>
2025-12-14 18:21:03 +0530

    done

--- Stacks/validparanthesis.java
+++ Stacks/validparanthesis.java
@@ -1,31 +1,38 @@
+package Stacks;
+
 import java.util.Stack;
 
 public class validparanthesis {
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
+        String str = "(())"; // Example input
         Stack<Character> s = new Stack<>();
-        for(int i=0; i<str.length(); i++){
-            char ch = str.charAt(i); 
-            if(ch=='('||ch=='{'||ch=='['){
-                stack.push(ch);
-            } else {
-                if(s.isEmpty()){
+        
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            
+            if (ch == '(' || ch == '{' || ch == '[') {
+                s.push(ch);
+            } else if (ch == ')' || ch == '}' || ch == ']') {
+                if (s.isEmpty()) {
                     System.out.println("false");
-                    return; 
+                    return;
                 }
-                if(s.peek()=='('&&ch==')'||s.peek()=='{'&&ch=='}'||s.peek()=='['&&ch==']'){
+                
+                if ((s.peek() == '(' && ch == ')') || 
+                    (s.peek() == '{' && ch == '}') || 
+                    (s.peek() == '[' && ch == ']')) {
                     s.pop();
                 } else {
-                    System.out.println("false");
-                    return; 
-                }else{
                     System.out.println("false");
                     return;
                 }
             }
-
+        }
+        
+        if (s.isEmpty()) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
         }
     }
-
-    
 }
