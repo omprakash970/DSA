fddaeca22a9c155f20c32af4d69996565d725f84
Bandi Om Prakash <omprakashbandi583@gmail.com>
2025-12-14 23:20:06 +0530

    done

---
+++ Greedy/activityselection.java
@@ -0,0 +1,15 @@
+package Greedy;
+import java.util.*; 
+
+import java.util.ArrayList;
+
+public class activityselection {
+    public static void main(String[] args){
+        int start[]={1,3,0,5,8,5};
+        int end[]={2,4,6,7,9,9};
+        int maxAct=0;
+        ArrayList<Integer> ans = new ArrayList<>()
+
+    }
+    
+}
