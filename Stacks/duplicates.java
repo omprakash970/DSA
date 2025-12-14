5264fd57d626d69f3066356f81625e3de3dedff4
Bandi Om Prakash <omprakashbandi583@gmail.com>
2025-12-14 20:07:35 +0530

    done

--- Stacks/duplicates.java
+++ Stacks/duplicates.java
@@ -1,33 +1,26 @@
 package Stacks;
-public static class duplicates(String str){
-    Stack<Character> s = new Stack<>(); 
-    for(int i =0;i<str.length();i++){
-        char ch =str.charAt(i); 
-        if(ch==')'){
-            int count =0; 
-            while(s.peek()!='('){
-                s.pop(); 
-                count++;
-            }
-            if(count<1){
-                return true; 
+import java.util.Stack;
 
-            }else{
-                s.pop(); 
+public class duplicates {
+    public static boolean checkDuplicates(String str) {
+        Stack<Character> s = new Stack<>(); 
+        for(int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i); 
+            if(ch == ')') {
+                int count = 0; 
+                while(s.peek() != '(') {
+                    s.pop(); 
+                    count++;
+                }
+                if(count < 1) {
+                    return true; 
+                } else {
+                    s.pop(); 
+                }
+            } else {
+                s.push(ch); 
             }
-        }else{
-            s.push(ch); 
-            
         }
+        return false;
     }
-
-
-}
-
-
-
-
-public class duplicateparanthesis {
-
-    
 }
