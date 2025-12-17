6dec89a8c36b808a57b926bcadf6a962ffddb861
Bandi Om Prakash <omprakashbandi583@gmail.com>
2025-12-17 22:08:22 +0530

    done

--- Greedy/Indiancoins.java
+++ Greedy/Indiancoins.java
@@ -1,25 +1,27 @@
-import java.util.ArrayList;
-import java.util.Comparator;
-
+import java.util.*;
+import java.Comparator;
 public class Indiancoins {
     public static void main(String[] args) {
-        Integer[] coins ={1,2,5,10,20,50,100,200,500,2000};
-        int amount =790; 
+        Integer[] coins = {1, 2, 5, 10, 20, 50, 100, 200, 500, 2000};
+        int amount = 790;
+
         Arrays.sort(coins, Comparator.reverseOrder());
-        int count =0; 
-        ArrayList<Integer> usedCoins = new ArrayList<>();
-        for(int coin: coins){
-            if(coins[i]<=amount){
-                while(coin<=amount){
-                    amount-=coin; 
-                    count++; 
-                    usedCoins.add(coin);
-                }
+
+        int count = 0;
+        List<Integer> usedCoins = new ArrayList<>();
+
+        for (int coin : coins) {
+            while (coin <= amount) {
+                amount -= coin;
+                count++;
+                usedCoins.add(coin);
             }
+            if (amount == 0) break;
         }
 
-         
+        System.out.println("Used coins: " + usedCoins);
+        System.out.println("Total coins: " + count);
+        if (amount > 0) System.out.println("Remaining amount: " + amount);
     }
 }
-    
 
