382a9a43d8d4a79c7c7db8aa156d3dffbfe4f678
Bandi Om Prakash <omprakashbandi583@gmail.com>
2025-12-17 22:52:32 +0530

    done

--- Strings/io.java
+++ Strings/io.java
@@ -9,11 +9,11 @@ public class io {
     public static boolean  isPalindrome(String str){
         int n=str.length(); 
         for(int i =0;i<n/2;i++){
-            if(str.charAt(i)==str.charAt(n-i-1)){
-                continue; 
+            if(str.charAt(i)!=str.charAt(n-i-1)){
+                return false;  
             }
             else{
-                return false; 
+                return true; 
             }
         }
         return true;
