f5197bf9d071d1a94c62290a5e0eb105b43243cf
Bandi Om Prakash <omprakashbandi583@gmail.com>
2025-12-17 22:35:50 +0530

    done

---
+++ Strings/io.java
@@ -0,0 +1,23 @@
+
+package Strings;
+
+import java.util.Scanner;
+
+
+public class io {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name; 
+        //input method
+        name=sc.nextLine(); 
+        System.out.println(name);
+        String firstname="Om Prakash"; 
+        String lastname=" Bandi"; 
+        //concatination method
+        String fullname=firstname.concat(lastname);
+        System.out.println(fullname);
+        System.out.println(fullname.length());
+        //String Atmethod
+    }
+    
+}
