d0a6d1f465a6c8fe0214ccadb79d31560057ad15
Bandi Om Prakash <omprakashbandi583@gmail.com>
2025-12-23 21:36:11 +0530

    done

---
+++ Binary_trees/Binary trees(part 2)/diameter.java
@@ -0,0 +1,18 @@
+public class diameter {
+    public class Node{
+        int val; 
+        Node left;
+        Node right;
+        Node(int val){
+            this.val=val;
+            this.left=null;
+            this.right=null;
+        }
+        
+    }
+    public static void main(String[] args){
+        System.out.println("Diameter of binary tree");
+        
+    }
+    
+}
