78be394e11d8209bd4762b82c3bd8c8943df26ce
Bandi Om Prakash <omprakashbandi583@gmail.com>
2025-12-27 15:19:41 +0530

    done

--- LinkedList_part1/Problems/problem2.java
+++ LinkedList_part1/Problems/problem2.java
@@ -1,8 +1,15 @@
+
+import java.util.LinkedList;
+
 //detect a loop/cycle in a linked list
 public class problem2 {
     public static void main(String[] args){
-        int[] arr = new int[]
+        LinkedList<Integer> list = new LinkedList<>();
+       
+        System.out.println(detectLoop(list) ? "Loop detected" : "No loop detected");
+
         
     }
+    
     
 }
