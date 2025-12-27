7917f62f0bd96e988a017f5ade2edfc2f04f5826
Bandi Om Prakash <omprakashbandi583@gmail.com>
2025-12-27 15:40:04 +0530

    cycle detection

--- LinkedList_part1/Problems/problem2.java
+++ LinkedList_part1/Problems/problem2.java
@@ -1,13 +1,31 @@
 
-import java.util.LinkedList;
-
-//detect a loop/cycle in a linked list
 public class problem2 {
-    public static void main(String[] args){
-        LinkedList<Integer> list = new LinkedList<>();
-       
-        System.out.println(detectLoop(list) ? "Loop detected" : "No loop detected");
+    public static class Node{
+        int data; 
+        Node next;
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
 
+
+
+
+    public boolean isCycle(){
+        Node slow=head; 
+        Node fast=head;
+        while(fast!=null&&fast.next!=null){
+            slow=slow.next; 
+            fast=fast.next.next;
+            if(slow==fast){
+                return true; 
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args){
+       
         
     }
     
