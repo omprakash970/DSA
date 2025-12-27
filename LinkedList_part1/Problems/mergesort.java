717eaab059b82f7a08cd5e3635acf8b3fefe735c
Bandi Om Prakash <omprakashbandi583@gmail.com>
2025-12-27 22:21:03 +0530

    done

--- LinkedList_part1/Problems/mergesort.java
+++ LinkedList_part1/Problems/mergesort.java
@@ -1,4 +1,20 @@
 public class mergesort {
+    public static class Node{
+        int data; 
+        Node next; 
+        Node(int data){
+            this.data=data; 
+            this.next=null; 
+        }
+    }
+    public Node mergeSort(Node head){
+        //find mid
+        Node mid=findMid(head); 
+
+        //left & right MS
+
+        //Merge
+    }
     public static void main(String[] args) {
         
     }
