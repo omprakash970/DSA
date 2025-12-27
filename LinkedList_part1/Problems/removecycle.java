6d57b31c537be9b4664c92f7c728e04a56ceb639
Bandi Om Prakash <omprakashbandi583@gmail.com>
2025-12-27 21:32:11 +0530

    done

--- LinkedList_part1/Problems/removecycle.java
+++ LinkedList_part1/Problems/removecycle.java
@@ -11,29 +11,42 @@ public class removecycle {
      public static void removeCycle(){
         Node slow=head; 
         Node fast =head; 
-        boolean cycle=0; 
+        boolean cycle=false; 
         while(fast!=null&&fast.next!=null){
             slow=slow.next; 
             fast=fast.next.next; 
             if(fast==slow){
-                cycle=1; 
+                cycle=true; 
                 break;
             }
         }
         if(!cycle){
             return; 
         }
+        //find the meeting point 
         slow=head; 
+        Node prev=null; 
         while(slow!=fast){
             prev=fast; 
             slow=slow.next; 
+            if(fast==null){
+                return; 
+            }
             fast=fast.next; 
         }
+
+        //removing the last node of the cycle
         prev.next=null; 
      }
 
 
     public static void main(String[] args){
+        head = new Node(1); 
+        head.next = new Node(2);
+        head.next.next = new Node(3);   
+        head.next.next.next = head.next; // Creating a cycle for testing
+        removeCycle();
+        System.out.println(head.data);
 
 
         
