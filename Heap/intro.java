66367ad0fa7a26764e8f952698606309bc1a37d2
Bandi Om Prakash <omprakashbandi583@gmail.com>
2026-01-02 19:31:19 +0530

    done

--- Heap/intro.java
+++ Heap/intro.java
@@ -4,11 +4,15 @@ import java.util.PriorityQueue;
 
 public class intro {
     public static void main(String[] args) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
         pq.add(5); 
         pq.add(4); 
         pq.add(1);
         pq.add(7); 
+        while(!pq.isEmpty()){
+            System.out.println(pq.peek());
+            pq.remove();
+        }
     }
 }
     
