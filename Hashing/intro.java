abf4c85895915765c10d5fde82b1860d0c3bd677
Bandi Om Prakash <omprakashbandi583@gmail.com>
2026-01-05 16:22:50 +0530

    done

--- Hashing/intro.java
+++ Hashing/intro.java
@@ -1,10 +1,15 @@
 
+import java.util.HashMap;
+
+
 
 
 public class intro {
     public static void main(String[] args){
 
-        System.out.println("Hellow world");
+        HashMap<String, Integer> Hm= new HashMap<>();
+        Hm.put("India", 130);
+        System.out.println(Hm);
     }
     
 }
