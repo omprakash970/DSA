5222bca6813574501d67e3cc8136a553526a6e97
Bandi Om Prakash <omprakashbandi583@gmail.com>
2026-01-05 16:23:33 +0530

    done

--- Hashing/intro.java
+++ Hashing/intro.java
@@ -9,6 +9,8 @@ public class intro {
 
         HashMap<String, Integer> Hm= new HashMap<>();
         Hm.put("India", 130);
+        Hm.put("China", 150);
+        Hm.put("USA", 40);
         System.out.println(Hm);
     }
     
