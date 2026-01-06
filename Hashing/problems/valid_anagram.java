e3ec0f490a033ca8e8fb3b1a4563a8508960a391
Bandi Om Prakash <omprakashbandi583@gmail.com>
2026-01-06 20:55:27 +0530

    done

--- Hashing/problems/valid_anagram.java
+++ Hashing/problems/valid_anagram.java
@@ -1,8 +1,27 @@
 package problems;
 
 public class valid_anagram {
-    
+    static boolean valid anagram(String s1, string s2){
+        if(s1.length() != s2.length()){
+            return false;
+        }
+        int freq[] = new int[26];
+        for(int i=0;i<s1.length();i++){
+            freq[s1.charAt(i) - 'a']++;
+            freq[s2.charAt(i) - 'a']--;
+        }
+        for(int count : freq){
+            if(count != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+
     public static void main(String[] args) {
+
 
     }
     
