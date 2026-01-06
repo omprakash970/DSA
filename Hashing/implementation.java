3d3e2fc9f4447a9f2d4f57872f609413fcc164a5
Bandi Om Prakash <omprakashbandi583@gmail.com>
2026-01-06 17:20:29 +0530

    done

--- Hashing/implementation.java
+++ Hashing/implementation.java
@@ -1,5 +1,27 @@
 
+import java.util.LinkedList;
+
+
 class implementation {
+    static class HashMap<K,V>{
+        private class Node{
+            private int size; 
+            private LinkedList<Node> buckets[]; 
+            @SuppressWarnings("unchecked");
+            public HashMap(){
+                public void put(K key, V value){
+
+                }
+                public boolean containsKey(K key){
+
+                }
+                
+            }
+        }
+    }
+
+
+
     public static void main(String[] args) {
  
     }
