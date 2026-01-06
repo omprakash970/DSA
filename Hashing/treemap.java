e59f4711ec626791362071534c5a6e07097649a7
Bandi Om Prakash <omprakashbandi583@gmail.com>
2026-01-06 18:07:57 +0530

    done

---
+++ Hashing/treemap.java
@@ -0,0 +1,14 @@
+
+import java.util.TreeMap;
+
+public class treemap {
+    public static void main(String[] args) {
+        TreeMap<String, Integer> Hm= new TreeMap<>();
+        Hm.put("India", 100);
+        Hm.put("China",150);
+        Hm.put("USA", 33);
+        Hm.put("Indonesia", 10);
+        System.out.println(Hm);
+    }
+    
+}
