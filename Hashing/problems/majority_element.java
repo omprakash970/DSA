c578688d3cba760e02b1dbf2b05946e55f5caf1d
Bandi Om Prakash <omprakashbandi583@gmail.com>
2026-01-06 20:45:06 +0530

    done

--- Hashing/problems/majority_element.java
+++ Hashing/problems/majority_element.java
@@ -3,14 +3,19 @@ import java.util.HashMap;
 
 public class majority_element{
     static int majority_element(int arr[]){
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i=0;i<arr.length;i++){
-            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
-            if(map.get(arr[i]) > arr.length/2){
-                return arr[i];
-            }
+      HashMap<Integer, Integer> Map = new HashMap<>();
+      for(int i=0;i<arr.length;i++){
+        if(Map.containsKey(arr[i])){
+            Map.put(arr[i], Map.get(arr[i])+1);
+        }else{
+            Map.put(arr[i], 1);
         }
-        return -1;
+        if(Map.get(arr[i]) > arr.length/2){
+            return arr[i];
+        }   
+      
+      }
+      return -1;
     }
 
     public static void main(String[] args) {
---
+++ Hashing/problems/valid_anagram.java
@@ -0,0 +1,9 @@
+package problems;
+
+public class valid_anagram {
+    
+    public static void main(String[] args) {
+
+    }
+    
+}
