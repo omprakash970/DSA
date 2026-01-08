79b48e422e1b7d381675a7b537058583d9121268
Bandi Om Prakash <omprakashbandi583@gmail.com>
2026-01-08 15:07:28 +0530

    done

--- Binary_trees/Binary trees(part 1)/preorder.java
+++ Binary_trees/Binary trees(part 1)/preorder.java
@@ -9,6 +9,19 @@ public class preorder {
             this.left=null;
         }
     }
+    static class BinaryTree{
+       static int idx=-1;
+       public static Node buildTree(int nodes[]){
+        idx++;; 
+        if(nodes[idx]==-1){
+            return null; 
+        }
+        Node newNode = new Node(nodes[idx]);
+        newNode.left=buildTree(nodes);
+        newNode.right=buildTree(nodes);
+        return newNode; 
+       }
+    }
 
 
     public static void main(String[] args) {
