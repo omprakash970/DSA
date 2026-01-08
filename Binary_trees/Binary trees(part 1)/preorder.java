6baf41e104beece2b29f3a902f31ea7360ca83b4
Bandi Om Prakash <omprakashbandi583@gmail.com>
2026-01-08 15:05:01 +0530

    done

---
+++ Binary_trees/Binary trees(part 1)/preorder.java
@@ -0,0 +1,18 @@
+public class preorder {
+    class  Node{
+        int data; 
+        Node left;
+        Node right; 
+        Node(int data){
+            this.data=data; 
+            this.right=null;
+            this.left=null;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
--- Hashing/problems/longest_Subarray_with_sum_key.java
+++ Hashing/problems/longest_Subarray_with_sum_key.java
@@ -1,5 +1,8 @@
 package problems;
 
 public class longest_Subarray_with_sum_key {
-    
+    public static void main(String[] args) {
+        
+    }
 }
+    
