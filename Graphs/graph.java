d88bdbe4e44a37d4556771a09770d137eca1fe45
Bandi Om Prakash <omprakashbandi583@gmail.com>
2026-01-21 19:36:10 +0530

    OKAY

---
+++ Graphs/graph.java
@@ -0,0 +1,22 @@
+package Graphs;
+
+import java.util.ArrayList;
+
+public class graph {
+    static class Edge{
+        int src; 
+        int dest; 
+        public Edge(int s, int d){
+            this.src=s; 
+            this.dest=d; 
+        }
+    }
+    public static void main(String[] args){
+
+        int V=5;
+        ArrayList<Edge>[]graph=new  ArrayList[V];
+        
+        
+    }
+}
+    
