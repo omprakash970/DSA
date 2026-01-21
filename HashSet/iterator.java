35e5ba20e945985355ab0f0ab15fc4d0861fcfc3
Bandi Om Prakash <omprakashbandi583@gmail.com>
2026-01-21 10:07:06 +0530

    done

--- HashSet/iterator.java
+++ HashSet/iterator.java
@@ -4,14 +4,16 @@ import java.util.Iterator;
 import java.util.*; 
 
 public class iterator {
+
     public static void main(String[] args) {
+
         HashSet<String> set = new HashSet<>();
         set.add("India");
         set.add("China");
         set.add("USA");
         set.add("Indonesia");
         Iterator it = set.iterator();
-        while(it.hasNext()){
+        while(it.hasNext()) { 
             System.out.println(it.next());
         }
         
