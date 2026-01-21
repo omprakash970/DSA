33c3121a9bf65eeb5ffcecd6b0dddc04af5030a1
Bandi Om Prakash <omprakashbandi583@gmail.com>
2026-01-21 19:46:59 +0530

    done

--- Graphs/graph.java
+++ Graphs/graph.java
@@ -6,9 +6,11 @@ public class graph {
     static class Edge{
         int src; 
         int dest; 
-        public Edge(int s, int d){
+        int w;
+        public Edge(int s, int d, int w){
             this.src=s; 
             this.dest=d; 
+            this.w=w;
         }
     }
     public static void main(String[] args){
@@ -17,9 +19,36 @@ public class graph {
         ArrayList<Edge>[]graph=new  ArrayList[V];
         for(int i=0;i<V;i++){
             graph[i]= new ArrayList<>();
-            
-        }
 
+        }
+        // Build a simple undirected, weighted graph
+        addUndirectedEdge(graph, 0, 1, 5);
+        addUndirectedEdge(graph, 1, 2, 1);
+        addUndirectedEdge(graph, 2, 3, 3);
+        addUndirectedEdge(graph, 3, 4, 2);
+
+        // Print adjacency lists
+        printGraph(graph);
+
+    }
+
+    // Helper: add edge in both directions for undirected graph
+    static void addUndirectedEdge(ArrayList<Edge>[] graph, int src, int dest, int w) {
+        graph[src].add(new Edge(src, dest, w));
+        graph[dest].add(new Edge(dest, src, w));
+    }
+
+    // Helper: print adjacency list
+    static void printGraph(ArrayList<Edge>[] graph){
+        for(int v=0; v<graph.length; v++){
+            System.out.print(v + " -> ");
+            ArrayList<Edge> edges = graph[v];
+            for(int i=0; i<edges.size(); i++){
+                Edge e = edges.get(i);
+                System.out.print("(" + e.dest + ", w=" + e.w + ") ");
+            }
+            System.out.println();
+        }
     }
 }
     
