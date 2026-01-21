8ad5916ca691bcefed07ce3178410c4145f40fd7
Bandi Om Prakash <omprakashbandi583@gmail.com>
2026-01-21 19:38:21 +0530

    done

--- Graphs/graph.java
+++ Graphs/graph.java
@@ -15,8 +15,11 @@ public class graph {
 
         int V=5;
         ArrayList<Edge>[]graph=new  ArrayList[V];
-        
-        
+        for(int i=0;i<V;i++){
+            graph[i]= new ArrayList<>();
+            
+        }
+
     }
 }
     
