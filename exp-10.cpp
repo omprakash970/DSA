0676a20278bc96e6760a5a7d98aa95318e60c0eb
Bandi Om Prakash <omprakashbandi583@gmail.com>
2026-02-13 09:51:23 +0530

    done

---
+++ exp-10.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std; 
+int main(){
+    vector<string> inv;
+    int choice; 
+    while(true){
+        cout<<"1.Add\t2.Remove\t3.Show\t4.Exit\n";
+        cin>>choice; 
+        if(choice==1){
+            string it;
+            cout<<"Enter item name: ";
+            cin>>it;
+            inv.push_back(it);
+        }else if(choice==2){
+            if(!inv.empty()){
+                inv.pop_back();
+            }
+
+        }else if(choice==3){
+            for(const auto& item: inv){
+                cout<<item<<endl;
+            }
+        }
+    }
+    return 0;
+
+}
