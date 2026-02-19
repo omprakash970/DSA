86974f2b88f3546c09e32c8ec7a571bc5839f9d5
Bandi Om Prakash <omprakashbandi583@gmail.com>
2026-02-19 17:50:02 +0530

    done

--- BackTracking/Classroom.java
+++ BackTracking/Classroom.java
@@ -1,9 +1,23 @@
-package BackTracking;
+
 
 public class Classroom {
+    public static void changeArr(int arr[], int i, int val){
+        if(i==arr.length){
+            return; 
+        }
+
+        arr[i]=val; 
+        changeArr(arr, i+1, val+1);
+        arr[i]=arr[i]-2;
+    }
+
     public static void main(String args[]){
         int arr[]= new int[5]; 
-        
+        changeArr(arr, 0, 1);
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+
     }
     
 }
