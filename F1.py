7e86d93ebd63fe7becf4c459a047c7724dfd08ef
Bandi Om Prakash <omprakashbandi583@gmail.com>
2026-02-20 12:09:10 +0530

    done

--- F1.py
+++ F1.py
@@ -2,8 +2,7 @@ import json
 def lambda_handler(event, content): 
     print("Lambda received: ", event)
     final_result= {
-        "final_message": "Hello from Lambda!",
-        "final_value":event["value"]*2
-
+        "message": "Hello from Lambda 1",
+        "value": event.get("value", 0)+10
     }
     return final_result
