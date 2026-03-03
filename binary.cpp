75b83ded82a1f7624a66b03e93ca347686f1f0e8
Bandi Om Prakash <omprakashbandi583@gmail.com>
2026-03-03 14:48:09 +0530

    done

--- binary.cpp
+++ binary.cpp
@@ -69,7 +69,11 @@ void levelOrder(Node* node){
     queue<Node*>q; 
     q.push(node); 
     while(!q.empty()){
-        
+        Node*node=q.front(); 
+        q.pop();
+        cout<<node->data<<" ";
+        if(node->left) q.push(node->left);
+        if(node->right) q.push(node->right);
     }
 
 }
