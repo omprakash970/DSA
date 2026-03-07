166230c6d4061f24ff9a5ceed4cfd65e9d33eadc
Bandi Om Prakash <omprakashbandi583@gmail.com>
2026-03-08 01:16:06 +0530

    done

--- Stacks/usingArraylist.java
+++ Stacks/usingArraylist.java
@@ -22,5 +22,18 @@ class usingArrayList{
             int top=list.get(list.size()-1);
             return top;
         }
+
+        public static void main(String[] args){
+            usingArrayList stack = new usingArrayList();
+            stack.push(1);
+            stack.push(2);
+            stack.push(3);
+            System.out.println(stack.peek());
+            System.out.println(stack.pop());
+            System.out.println(stack.peek());
+
+
+
+        }
 }
 
