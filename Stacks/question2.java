32458244ac614b7c7a8114baf8095917cbc02e5a
Bandi Om Prakash <omprakashbandi583@gmail.com>
2026-03-08 01:40:35 +0530

    done

--- Stacks/question2.java
+++ Stacks/question2.java
@@ -1,5 +1,25 @@
 //reverse a String using Stack 
+import java.util.*;
 
 public class question2 {
-    
+public String reverseString(String str){
+    Stack<Character>s = new Stack<>();
+    for(int i=0;i<str.length();i++){
+        s.push(str.charAt(i));
+    }
+    StringBuilder sb = new StringBuilder("");
+    while(!s.isEmpty()){
+        sb.append(s.pop());
+    }
+    return sb.toString();
 }
+
+
+    public static void main(String[] args) {
+        question2 q = new question2();
+        String str = "Hello World";
+        String reversed = q.reverseString(str);
+        System.out.println(reversed);
+    }
+}
+    
