f5fae02255e419132f8b4600a99efb12c9eed42d
Bandi Om Prakash <omprakashbandi583@gmail.com>
2026-03-10 00:25:16 +0530

    done

--- CPP(STL)/problem-1.cpp
+++ CPP(STL)/problem-1.cpp
@@ -1,13 +1,23 @@
-//reverse an Array using STL 
-#include <iostream>
-#include <algorithm>
-#include <vector>
-using namespace std;
-int main(){
-    vector<int> arr={1,2,3,4,5};
-    reverse(arr.begin(),arr.end());
-    for(int i=0;i<arr.size();i++){
-        cout<<arr[i]<<" ";
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std; 
+int maxSum(vector<int> &arr, int k){
+    int sum=0; 
+    for(int i=0;i<k;i++){
+        sum+=arr[i];
     }
-    
+    int ans=sum; 
+    for(int i=k;i<arr.size();i++){
+        
+    }
+
 }
+
+
+
+int main(){
+
+}
+
