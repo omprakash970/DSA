3326fb7e684107a033519da9d7d25b654af6aaf4
Bandi Om Prakash <omprakashbandi583@gmail.com>
2026-03-09 17:31:36 +0530

    done

---
+++ CPP(STL)/problem-1.cpp
@@ -0,0 +1,13 @@
+//reverse an Array using STL 
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int main(){
+    vector<int> arr={1,2,3,4,5};
+    reverse(arr.begin(),arr.end());
+    for(int i=0;i<arr.size();i++){
+        cout<<arr[i]<<" ";
+    }
+    
+}
