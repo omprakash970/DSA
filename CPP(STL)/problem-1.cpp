917bee82eaa7f717f5bb090b5acb201b9ae5b27b
Bandi Om Prakash <omprakashbandi583@gmail.com>
2026-03-10 00:26:15 +0530

    done

--- CPP(STL)/problem-1.cpp
+++ CPP(STL)/problem-1.cpp
@@ -10,14 +10,19 @@ int maxSum(vector<int> &arr, int k){
     }
     int ans=sum; 
     for(int i=k;i<arr.size();i++){
-        
+        sum+=arr[i];
+        sum-=arr[i-k];
+        ans=max(ans,sum);
     }
-
+    return ans;
 }
 
 
 
 int main(){
+    vector<int> arr={1,2,3,4,5,6,7,8,9};
+    int k=3; 
+    cout<<maxSum(arr,k)<<endl;
 
 }
 
