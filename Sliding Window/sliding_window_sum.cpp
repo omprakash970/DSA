bbd0fafc127fb4bc17a3a158ca2765f32ed2dcb2
Bandi Om Prakash <omprakashbandi583@gmail.com>
2026-03-21 12:20:59 +0530

    done

---
+++ Sliding Window/sliding_window_sum.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+/*
+You are given an array of n integers. Your task is to calculate the sum of each window of k elements, from left to right.
+In this problem the input data is large and it is created using a generator.
+Input
+The first line contains two integers n and k: the number of elements and the size of the window.
+The next line contains four integers x, a, b and c: the input generator parameters. The input is generated as follows:
+
+x_1=x
+x_i=(ax_{i-1}+b) \bmod c for i=2,3,\dots,n
+
+Output
+Print the xor of all window sums.
+Constraints
+
+1 \le k \le n \le 10^7
+0 \le x, a, b \le 10^9
+1 \le c \le 10^9
+
+Example
+Input:
+8 5
+3 7 1 11
+
+Output:
+12
+
+Explanation: The input array is [3,0,1,8,2,4,7,6]. The windows are [3,0,1,8,2], [0,1,8,2,4], [1,8,2,4,7] and [8,2,4,7,6], and their sums are 14, 15, 22 and 27. Thus, the answer is 14 \oplus 15 \oplus 22 \oplus 27 = 12.
+
+
+
+
+
+
+
+
+*/
+
+#include<vector>
+using namespace std;
+int main(){
+    int t; 
+    cin>>t;
+    while(t--){
+        int n, k; 
+        cin>>n>>k;
+        vector<int> arr(n);
+        for(int i=0; i<n; i++){
+            cin>>arr[i];
+        }
+        vector<int> sum;
+        int window_sum = 0;
+        for(int i=0; i<k; i++){
+            window_sum += arr[i];
+        }
+        sum.push_back(window_sum);
+        for(int i=k; i<n; i++){
+            window_sum += arr[i] - arr[i-k];
+            sum.push_back(window_sum);
+        }
+        int result = 0;
+        for(int s : sum){
+            result ^= s;
+        }
+        //commit okay
+        cout<<result<<endl;
+        
+
+
+    }
+}
