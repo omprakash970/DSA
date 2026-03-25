ef2f022051ea981037ab2d14fbb618625a84e647
Bandi Om Prakash <omprakashbandi583@gmail.com>
2026-03-25 14:41:58 +0530

    done

---
+++ Arrays/problem-2.cpp
@@ -0,0 +1,24 @@
+//given two strings, write a function to check if other string is permutation of the first string.
+#include<iostream>
+#include<unordered_map>
+#include<vector>
+using namespace std;
+class Solution{
+    public:
+    bool isPermuation(string s, string t){
+        unordered_map<char, int>m; 
+        for(int i=0;i<s.size();i++){
+            m[s[i]]++; 
+        }
+       for(int j=0;j<t.size();j++){
+        if(s.find(t[j])==s.end()){
+            return false; 
+        }
+        m[t[j]]--;
+        if(m[t[j]]<0){
+            return false; 
+        }
+       }
+        return true;
+    }
+    };
